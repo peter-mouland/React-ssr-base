e703f406ad563674990e5ad0860783b70836741f
Peter Mouland <peter.mouland@gmail.com>
2017-02-16 21:42:54 +0000

    regfactor nightwatch pageLoaded command

--- tests/config/nightwatch-commands/pageLoaded.js
+++ tests/config/nightwatch-commands/pageLoaded.js
@@ -1,16 +1,47 @@
-exports.command = function pageLoaded(page, waitFor, callback) {
-  var browser = this;
-  var url = browser.globals.TARGET_PATH;
+const util = require('util');
+const events = require('events');
+const debug = require('debug');
 
-  url += (page) ? page : "";
+const log = debug('pageLoaded');
+let browser;
+
+function pageLoaded() {
+  events.EventEmitter.call(this);
+  browser = this.api;
+}
+util.inherits(pageLoaded, events.EventEmitter);
+
+pageLoaded.prototype.complete = function complete({ e, done }) {
+  if (e) {
+    log('e', e);
+  }
+  this.emit('complete');
+  if (typeof done === 'function') {
+    done();
+  }
+};
+
+pageLoaded.prototype.command = function pageLoadedFn(page, opts = {}) {
+  const { selector, disableAnimations, cookie, done } = opts;
+  const url = browser.globals.TARGET_PATH + (page || '');
+  log(url);
+  const args = [disableAnimations ? 'disable-animations' : ''];
+  function disableAnimationFunction(className) {
+    document.body.className += ` ${className}`;
+    return document.body.className;
+  }
 
   browser
+    .windowMaximize()
     .url(url)
-    .waitForElementVisible(waitFor || '#html', 10000, function(){
-      if (typeof callback === 'function') {
-        callback.call(browser);
-      }
+    .setCookie(cookie)
+    .url(url)
+    .waitForElementVisible(selector || 'body', 10000)
+    .execute(disableAnimationFunction, args, () => {
+      this.complete({ done });
     });
 
   return this;
 };
+
+module.exports = pageLoaded;
