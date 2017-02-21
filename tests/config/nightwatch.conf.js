74dd60b970eece836e03b7b4503e5b43be097779
Peter Mouland <peter.mouland@gmail.com>
2017-02-21 23:53:18 +0000

    now fix xb-tests

--- tests/config/nightwatch.conf.js
+++ tests/config/nightwatch.conf.js
@@ -16,19 +16,20 @@ hook('.svg', (source) => {
   const markup = SvgLoader.getExtractedSVG(source, { removeSVGTagAttrs: false });
   return `module.exports =  ${JSON.stringify(markup)}`;
 });
+const needLocalServer = !argv.target;
+let db;
 
-// Connect to test DB (needed for functional tests)
-const db = require('../../src/server/models');
-const config = require('./db.json');
-db.connect(config.dbUri);
-const loadFixtures = require('./fixtures');
 
 // build assets array from webpack bundle for test pages
 const webpackAssets = require('../../compiled/webpack-assets.json');
 const mapWebpackAssets = require('../../src/server/utils/mapWebpackAssets');
 const assets = mapWebpackAssets(webpackAssets);
-const needLocalServer = !argv.target;
 const startLocalServers = (done) => {
+  // Connect to test DB (needed for functional tests)
+  db = require('../../src/server/models');
+  const config = require('./db.json');
+  db.connect(config.dbUri);
+  const loadFixtures = require('./fixtures');
   loadFixtures().then(()=> {
     const createServer = require('../../src/server/server');
     openServer = createServer(assets).listen(process.env.PORT, () => {
@@ -42,16 +43,26 @@ const stopLocalServers = (done) => {
   openServer.close(() => db.connection.close(done));
 };
 const noop = (done) => { done(); };
+const TARGET_PATH = argv.target || `http://localhost:${process.env.PORT}`;
 let openServer;
 
 module.exports = (function(settings) {
+  var buildString = "";
+  if (argv.sha) {
+    buildString += argv.sha
+  } else {
+    buildString += 'local ' + Date.now();
+    buildString = buildString.substring(0, buildString.length - 4);
+  }
+
   settings.test_settings.default.globals = {
-    TARGET_PATH : argv.target || `http://localhost:${process.env.PORT}`,
+    TARGET_PATH : TARGET_PATH,
     before:  needLocalServer ? startLocalServers : noop,
     after: needLocalServer ? stopLocalServers : noop
   };
+  settings.test_settings.default.desiredCapabilities['browserstack.local'] = TARGET_PATH.indexOf('localhost') > -1;
   settings.test_settings.default.desiredCapabilities['browserstack.user'] = argv.bsuser || process.env.BROWSERSTACK_USER;
   settings.test_settings.default.desiredCapabilities['browserstack.key'] = argv.bskey || process.env.BROWSERSTACK_KEY;
-  settings.test_settings.default.desiredCapabilities['build'] = (argv.sha) ? argv.sha : ' local ';
+  settings.test_settings.default.desiredCapabilities['build'] = buildString;
   return settings;
 })(require('./nightwatch.json'));
--- tests/config/nightwatchGlobals.js
+++ tests/config/nightwatchGlobals.js
@@ -9,7 +9,7 @@ module.exports = {
 
   // default timeout value in milliseconds for waitFor commands and implicit waitFor value for
   // expect assertions
-  waitForConditionTimeout: 140000, // mobile testing takes ages!
+  waitForConditionTimeout: 14000, // mobile testing takes ages!
 
   // this will cause waitFor commands on elements to throw an error if multiple
   // elements are found using the given locate strategy and selector
@@ -17,5 +17,5 @@ module.exports = {
 
   // controls the timeout time for async hooks.
   // Expects the done() callback to be invoked within this time or an error is thrown
-  asyncHookTimeout: 140000, // mobile testing takes ages!
+  asyncHookTimeout: 14000, // mobile testing takes ages!
 };
